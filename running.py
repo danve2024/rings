adb6aef09e25aefcca1bcab4a96140ab36d1725c
Dany <james.blackwood.jr@gmail.com>
2025-01-03 18:21:21 +0300

    Requirements updated

--- running.py
+++ running.py
@@ -1,5 +1,6 @@
 from progress.bar import IncrementalBar
 from math import prod
+import time
 
 class Progress:
     @staticmethod
@@ -16,20 +17,40 @@ class Progress:
         self.parameters = parameters
         self.show_progress = show_progress
         self.bar = None
-        print(f'Estimated time: {(operation_time * self.len())//3600000}hrs')
+        self.start_time = time.time()
         self.init()
 
     @if_show
-    def init(self):
+    def init(self) -> None:
         self.bar = IncrementalBar(self.name, max=self.len())
 
-    def len(self):
+    def len(self) -> int:
         return prod(len(item) for item in self.parameters)
 
+    @staticmethod
+    def format_time(seconds) -> str:
+        hrs, rem = divmod(seconds, 3600)
+        mins, secs = divmod(rem, 60)
+        return f"{int(hrs):02}:{int(mins):02}:{int(secs):02}"
+
     @if_show
-    def next(self, n=1):
+    def next(self, n=1) -> None:
+        elapsed_time = time.time() - self.start_time
+        completed = self.bar.index + n
+        total = self.len()
+        if completed > 0:
+            estimated_total_time = (elapsed_time / completed) * total
+        else:
+            estimated_total_time = 0
+
+        elapsed_formatted = self.format_time(elapsed_time)
+        total_formatted = self.format_time(estimated_total_time)
+
+        self.bar.suffix = f" {self.bar.index}/{self.bar.max} [{elapsed_formatted}/{total_formatted}]"
         self.bar.next(n)
 
     @if_show
-    def finish(self):
+    def finish(self) -> None:
         self.bar.finish()
+        total_time = time.time() - self.start_time
+        print(f'Total time elapsed: {self.format_time(total_time)}')
